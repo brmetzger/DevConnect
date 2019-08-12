af9e47afbb8a03b15665e4dd41eacddcac8add19
gitrog <37446909+gitrous@users.noreply.github.com>
2019-08-12 15:32:27 -0400

    Allow users to search for developers
    It's just me! Hire me!

---
+++ js/developers.js
@@ -0,0 +1,13 @@
+const DEVELOPERS = [
+    {
+        "Username":"gitrog",
+        "UserId":156875176,
+        "Jobs":["Programmer","Translator","Web Developer"],
+        "Description":"Hi I'm gitrog, I'll add more to my description later."
+    }
+];
+
+//Send the developers to other scripts
+function getDevs() {
+    return DEVELOPERS;
+};
---
+++ js/search.js
@@ -0,0 +1,67 @@
+const CREATOR_TYPES = [
+    "",
+    "3D Modeler",
+    "Builder",
+    "Composer",
+    "GFX Artist",
+    "Programmer",
+    "Translator",
+    "Web Developer"
+];
+
+let developers = getDevs();
+
+//Setup the search page
+function setup() {
+    //Show the creator types
+    let creatorList = document.getElementById("creatorTypes");
+    for (let i in CREATOR_TYPES) {
+        creatorList.innerHTML += `
+        <option value="${CREATOR_TYPES[i].toLowerCase()}" onClick="sortBy('${CREATOR_TYPES[i]}')">
+            ${CREATOR_TYPES[i]}
+        </option>`;
+    };
+};
+
+//Search for developers
+let category;
+function search() {
+    //Reset the page
+    let devList = document.getElementById("devs");
+    devList.innerHTML = "";
+
+    if (category && category != "") {
+        //Show all of the developers in a category
+        for (let i in developers) {
+            let devInfo = developers[i];
+            for (let j in devInfo.Jobs) {
+                if (devInfo.Jobs[j] == category) {
+                    //Developer meets the category
+                    devList.innerHTML += `
+                    <div class="dev">
+                        <h1 class="devHead">${devInfo.Username}</h1>
+                        <h2 class="devHead">${devInfo.Jobs[0]}</h1>
+                        <img class="devPreview" src="https://www.roblox.com/headshot-thumbnail/image?userId=${devInfo.UserId}&width=420&height=420&format=png"/>
+                    </div>`;
+                    break;
+                };
+            };
+        };
+    } else {
+        //Show all of the developers
+        for (let i in developers) {
+            let devInfo = developers[i];
+            devList.innerHTML += `
+            <div class="dev">
+                <h1 class="devHead">${devInfo.Username}</h1>
+                <h2 class="devHead">${devInfo.Jobs[0]}</h1>
+                <img class="devPreview" src="https://www.roblox.com/headshot-thumbnail/image?userId=${devInfo.UserId}&width=420&height=420&format=png"/>
+            </div>`;
+        };
+    };
+};
+
+//Sort by a category
+function sortBy(sortCat) {
+    category = sortCat;
+};
