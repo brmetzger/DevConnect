382e449045213a0fa1ce4d9d01095215f18c98fe
gitrog <37446909+gitrous@users.noreply.github.com>
2019-08-13 17:47:54 -0400

    Create user profiles

--- js/developers.js
+++ js/developers.js
@@ -3,15 +3,26 @@ const DEVELOPERS = [
         "Username":"gitrog",
         "UserId":156875176,
         "Jobs":["Programmer","Translator","Web Developer","UI Designer","3D Modeler"],
-        "Description":`Hi! My name is gitrog, I am a seventeen year old game developer. My native language is English, although I do translations for Serbian. On Roblox, I primarily do scripting and programming, but I can also do UI Design and 3D Modeling. Prices are negotiable, but USD only please.`,
-        "Contacts":[["DISCORD",189714941228482560],["TWITTER","realgitrog"]]
+        "Description":`Hi! My name is gitrog, I am a seventeen year old student in a Northeastern American vocational high school and hobbyist game developer. I can speak English and Serbian, and I've been using Roblox since 2010, and programming since 2014.`,
+        "Skills":`I'm primarily a programmer/scripter, and I can program in Java, JavaScript, Python, VisualBasic, and of course, Lua. However, I also do web development, this website is a great example of my work. I also do translations from English into Serbian, and if I need to, I can do 3D Modeling and UI Design, however I wouldn't consider myself a professional in either field.`,
+        "Examples":[
+            {"Name":"Downtown Chronicles","Link":"https://www.roblox.com/games/1291467486/Downtown-Chronicles-OPEN-BETA"},
+            {"Name":"Bloxell: Origins","Link":"https://www.roblox.com/games/2653361429/Bloxell-Origins-Paid-Beta"}
+        ],
+        "Rate":"$12 per hour, flat rates can be negotiated.",
+        "Contacts":{"DISCORD":189714941228482560,"TWITTER":"realgitrog"}
     },
     {
         "Username":"posatta",
         "UserId":17828104,
         "Jobs":["Programmer","Texture Artist"],
         "Description":`Hi I'm posatta, I'll add more to my description later.`,
-        "Contacts":[["DISCORD",211933202237882368],["TWITTER","posatta"]]
+        "Skills":`Hi I'm posatta, I'll add more to my skills later.`,
+        "Examples":[
+            {"Name":"Downtown Chronicles","Link":"https://www.roblox.com/games/1291467486/Downtown-Chronicles-OPEN-BETA"}
+        ],
+        "Rate":"Hi I'm posatta, I haven't decided on a rate yet.",
+        "Contacts":{"DISCORD":211933202237882368,"TWITTER":"posatta1"}
     }
 ];
 
--- js/search.js
+++ js/search.js
@@ -13,6 +13,90 @@ const CREATOR_TYPES = [
 
 let developers = getDevs();
 
+//Convert an array into an English string
+function arrayToEnglish(toTranslate) {
+    if (toTranslate.length <= 0) {
+        //Nothing in the list, return nothing
+        return "";
+    } else if (toTranslate.length == 1) {
+        //One item list, return the one item
+        return toTranslate[0];
+    } else if (toTranslate.length == 2) {
+        //Two items in the list, join them with "and"
+        return `${toTranslate[0]} and ${toTranslate[1]}`;
+    } else {
+        //Three or more items in the list, combine them with commas and the last with "and"
+        var outputString = "";
+        for (var i in toTranslate) {
+            outputString += toTranslate[i];
+            if (i == toTranslate.length - 2) {
+                //Second to last, add the ", and"
+                outputString += ", and ";
+            } else if (i < toTranslate.length - 2) {
+                //Add a comma
+                outputString += ", ";
+            };
+        };
+        return outputString;
+    };
+};
+
+//Open a user's profile
+function openProfile(userId) {
+    //Get the user data
+    let userData = developers[userId];
+
+    //Get examples of the user's work
+    function getExamples() {
+        let output = "";
+        for (let i in userData.Examples) {
+            output += `<a class="workExample" href="${userData.Examples[i].Link}" target="blank">${userData.Examples[i].Name}</a>&nbsp;&nbsp;`;
+        };
+        return output;
+    };
+
+    //Create buttons for the user's contact information
+    function getContacts() {
+        let output = `<a href="https://www.roblox.com/users/${userData.UserId}/profile" target="blank"><img src="assets/roblox.png" class="social-button"/></a>`;
+        if (userData.Contacts.TWITTER) {
+            output += `<a href="https://twitter.com/${userData.Contacts.TWITTER}" target="blank"><img src="assets/twitter.png" class="social-button"/></a>`;
+        };
+        if (userData.Contacts.DISCORD) {
+            output += `<a href="https://discordapp.com/users/${userData.Contacts.DISCORD}" target="blank"><img src="assets/discord.png" class="social-button"/></a>`;
+        };
+        return output;
+    };
+
+    //Setup the profile
+    let devList = document.getElementById("devs");
+    devList.innerHTML = `
+    <div class="profile">
+        <img src="https://www.roblox.com/bust-thumbnail/image?userId=${userData.UserId}&width=420&height=420&format=png" class="profileImage"/>
+        <br/>
+        <h1 class="floatHead">${userData.Username}</h1>
+        <h2 class="jobs">${arrayToEnglish(userData.Jobs)}</h1>
+        <h2 class="floatHead">About Me</h1>
+        <p class="profileBody" style="margin-left:260px;">
+            ${userData.Description}
+        </p>
+        <br style="clear:left;"/><br/>
+        <h2 class="profileHead">Skills</h2>
+        <p class="profileBody">
+            ${userData.Skills}
+        </p>
+        <h2 class="profileHead">Work Examples</h2>
+        <p class="profileBody">
+            ${getExamples()}
+        </p>
+        <h2 class="profileHead">Prices</h2>
+        <p class="profileBody">
+            ${userData.Rate}
+        </p>
+        <h2 class="profileHead">Contact Info</h2>
+        ${getContacts()}
+    </div>`;
+};
+
 //Setup the search page
 function setup() {
     //Show the creator types
@@ -32,6 +116,15 @@ function search() {
     let devList = document.getElementById("devs");
     devList.innerHTML = "";
 
+    function addDev(devInfo,i) {
+        devList.innerHTML += `
+        <div class="dev" onClick="openProfile(${i});">
+            <h1 class="devHead">${devInfo.Username}</h1>
+            <h2 class="devHead">${devInfo.Jobs[0]}</h1>
+            <img class="devPreview" src="https://www.roblox.com/headshot-thumbnail/image?userId=${devInfo.UserId}&width=420&height=420&format=png"/>
+        </div>`;
+    };
+
     if (category && category != "") {
         //Show all of the developers in a category
         for (let i in developers) {
@@ -39,12 +132,7 @@ function search() {
             for (let j in devInfo.Jobs) {
                 if (devInfo.Jobs[j] == category) {
                     //Developer meets the category
-                    devList.innerHTML += `
-                    <div class="dev">
-                        <h1 class="devHead">${devInfo.Username}</h1>
-                        <h2 class="devHead">${devInfo.Jobs[0]}</h1>
-                        <img class="devPreview" src="https://www.roblox.com/headshot-thumbnail/image?userId=${devInfo.UserId}&width=420&height=420&format=png"/>
-                    </div>`;
+                    addDev(devInfo,i);
                     break;
                 };
             };
@@ -52,15 +140,10 @@ function search() {
     } else {
         //Show all of the developers
         for (let i in developers) {
-            let devInfo = developers[i];
-            devList.innerHTML += `
-            <div class="dev">
-                <h1 class="devHead">${devInfo.Username}</h1>
-                <h2 class="devHead">${devInfo.Jobs[0]}</h1>
-                <img class="devPreview" src="https://www.roblox.com/headshot-thumbnail/image?userId=${devInfo.UserId}&width=420&height=420&format=png"/>
-            </div>`;
+            addDev(developers[i],i);
         };
     };
+    devList.innerHTML += "<br style='clear:left'/>";
 };
 
 //Sort by a category
