4cd1aacf9f324aa47a085742986bd14e0247baeb
gitrog <37446909+gitrous@users.noreply.github.com>
2019-08-13 21:22:59 -0400

    Add DevConnect to my portfolio

--- js/developers.js
+++ js/developers.js
@@ -7,7 +7,8 @@ const DEVELOPERS = [
         "Skills":`I'm primarily a programmer/scripter, and I can program in Java, JavaScript, Python, VisualBasic, and of course, Lua. However, I also do web development, this website is a great example of my work. I also do translations from English into Serbian, and if I need to, I can do 3D Modeling and UI Design, however I wouldn't consider myself a professional in either field.`,
         "Examples":[
             {"Name":"Downtown Chronicles","Link":"https://www.roblox.com/games/1291467486/Downtown-Chronicles-OPEN-BETA"},
-            {"Name":"Bloxell: Origins","Link":"https://www.roblox.com/games/2653361429/Bloxell-Origins-Paid-Beta"}
+            {"Name":"Bloxell: Origins","Link":"https://www.roblox.com/games/2653361429/Bloxell-Origins-Paid-Beta"},
+            {"Name":"DevConnect","Link":"http://devconnect.info"}
         ],
         "Rate":"$12 per hour, flat rates can be negotiated.",
         "Contacts":{"DISCORD":189714941228482560,"TWITTER":"realgitrog"}
